3a99cd4360860622e326883cd62cde0f231b1b20
Abdul Samad <abdul.samad4068@gmail.com>
2020-09-12 23:04:26 +0500

    Update roster.sql

--- Week 4 Peer graded assignment/roster.sql
+++ Week 4 Peer graded assignment/roster.sql
@@ -1,21 +1,10 @@
--- phpMyAdmin SQL Dumpages
--- version 5.0.2
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jun 04, 2020 at 02:05 AM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+
 
 --
 -- Database: `roster`
